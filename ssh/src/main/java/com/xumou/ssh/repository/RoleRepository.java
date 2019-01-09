2dc8210235fcaaab455681fe65269b0485226d82
xumou <1643469771@qq.com>
2019-01-09 18:02:48 +0800

    20190109

--- ssh/src/main/java/com/xumou/ssh/repository/RoleRepository.java
+++ ssh/src/main/java/com/xumou/ssh/repository/RoleRepository.java
@@ -2,11 +2,12 @@ package com.xumou.ssh.repository;
 
 import com.xumou.ssh.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 /**
  * JpaRepository 是基本增删改查
- * UserDao 是扩展用法, 随自己怎么用
+ * JpaSpecificationExecutor 时JPA提供的查询
  */
-public interface RoleRepository extends JpaRepository<Role,Long>{
+public interface RoleRepository extends JpaRepository<Role,Long>, JpaSpecificationExecutor<Role>{
 
 }
