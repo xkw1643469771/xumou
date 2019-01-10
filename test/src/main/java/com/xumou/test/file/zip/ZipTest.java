0e86b7e70adf97cb290bab4164781cc88b829411
xumou <1643469771@qq.com>
2019-01-10 17:34:59 +0800

    20190110

---
+++ test/src/main/java/com/xumou/test/file/zip/ZipTest.java
@@ -0,0 +1,34 @@
+package com.xumou.test.file.zip;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+/**
+ *
+ */
+public class ZipTest {
+
+    // 解压zip文件
+    @Test
+    public void zipTest() throws IOException {
+        ZipFile zipFile = new ZipFile(
+                new File("D:\\Downloads\\非材料类变更模板1.zip"),
+                Charset.forName("GBK")
+        );
+        Enumeration<? extends ZipEntry> entries = zipFile.entries();
+        while(entries.hasMoreElements()){
+            ZipEntry zipEntry = entries.nextElement();
+            System.out.println(zipEntry.getName());
+            InputStream inputStream = zipFile.getInputStream(zipEntry);
+        }
+    }
+
+}
