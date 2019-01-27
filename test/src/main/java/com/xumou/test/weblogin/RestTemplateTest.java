cf4a99d65d53cf647a25193eb88cf46573173af4
xumou <1643469771@qq.com>
2019-01-27 16:54:38 +0800

    20190127

--- test/src/main/java/com/xumou/test/weblogin/RestTemplateTest.java
+++ test/src/main/java/com/xumou/test/weblogin/RestTemplateTest.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpRequest;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.util.LinkedMultiValueMap;
@@ -21,18 +22,20 @@ import java.io.IOException;
  *
  */
 public class RestTemplateTest {
+
+    private RestTemplate restTemplate = new RestTemplate();
+
     /**
      * post基本方式请求
      */
     @Test
     public void postTest(){
-        RestTemplate rest = new RestTemplate();
+        String url = "http://localhost:9998/userTest/selectUserByParam";
         MultiValueMap<String, Object> paramMap = new LinkedMultiValueMap<>();
-        MultiValueMap<String, String> headerMap = new LinkedMultiValueMap<>();
         HttpHeaders header = new HttpHeaders();
         header.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity<>(paramMap, header);
-        String body = rest.postForObject("http://localhost:9998/userTest/selectUserByParam", httpEntity, String.class);
+        String body = restTemplate.postForObject(url, httpEntity, String.class);
         System.out.println(body);
     }
 
@@ -41,26 +44,44 @@ public class RestTemplateTest {
      */
     @Test
     public void executeTest(){
-        new RestTemplate().execute(
-                "http://localhost:9998/userTest/selectUserByParam",
-                HttpMethod.POST,
-                new RestTemplate() {
-                    public RequestCallback getRequestCallback() {
-                        MultiValueMap<String, Object> paramMap = new LinkedMultiValueMap<>();
-                        HttpHeaders header = new HttpHeaders();
-                        header.setContentType(MediaType.APPLICATION_JSON);
-                        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(paramMap, header);
-                        return super.httpEntityCallback(requestEntity, String.class);
-                    }
-                }.getRequestCallback(), new ResponseExtractor<String>() {
-                    public String extractData(ClientHttpResponse response) throws IOException {
-                        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                        FileCopyUtils.copy(response.getBody(), bos);
-                        System.out.println(new String(bos.toByteArray()));
-                        return "";
-                    }
-                }
-        );
+        String url = "http://localhost:9998/userTest/selectUserByParam";
+        restTemplate.execute(url, HttpMethod.POST, new RestTemplate() {
+            public RequestCallback getRequestCallback() {
+                MultiValueMap<String, Object> paramMap = new LinkedMultiValueMap<>();
+                HttpHeaders header = new HttpHeaders();
+                header.setContentType(MediaType.APPLICATION_JSON);
+                HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(paramMap, header);
+                return super.httpEntityCallback(requestEntity, String.class);
+            }
+        }.getRequestCallback(), new ResponseExtractor<String>() {
+            public String extractData(ClientHttpResponse response) throws IOException {
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                FileCopyUtils.copy(response.getBody(), bos);
+                System.out.println(new String(bos.toByteArray()));
+                return "";
+            }
+        });
+    }
+
+    /**
+     * 自定义请求2
+     */
+    @Test
+    public void executeTest2(){
+        String url = "http://localhost:9998/userTest/selectUserByParam";
+        String str = new RestTemplate().execute(url, HttpMethod.POST, new RequestCallback() {
+            public void doWithRequest(ClientHttpRequest request) throws IOException {
+                request.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+                request.getBody().write("{\"id\":1}".getBytes());
+            }
+        }, new ResponseExtractor<String>() {
+            public String extractData(ClientHttpResponse response) throws IOException {
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                FileCopyUtils.copy(response.getBody(), bos);
+                return new String(bos.toByteArray());
+            }
+        });
+        System.out.println(str);
     }
 
     /**
@@ -68,14 +89,12 @@ public class RestTemplateTest {
      */
     @Test
     public void exchange(){
-        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://localhost:9998/userTest/selectUserByParam";
         MultiValueMap<String, Object> paramMap = new LinkedMultiValueMap<>();
         HttpHeaders header = new HttpHeaders();
         header.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(paramMap, header);
-        ResponseEntity<String> responseEntity = restTemplate.exchange(
-                "http://localhost:9998/userTest/selectUserByParam",
-                HttpMethod.POST, requestEntity, String.class);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
         System.out.println(requestEntity.getBody());
     }
 
@@ -84,10 +103,8 @@ public class RestTemplateTest {
      */
     @Test
     public void getTest(){
-        RestTemplate restTemplate = new RestTemplate();
-        String str = restTemplate.getForObject(
-                "http://localhost:8080/mis/es/toSingleReimb.action?functionId=11018124&emptyMsg=false",
-                String.class);
+        String url = "http://localhost:8080/mis/es/toSingleReimb.action?functionId=11018124&emptyMsg=false";
+        String str = restTemplate.getForObject(url, String.class);
         System.out.println(str);
     }
 
@@ -102,7 +119,6 @@ public class RestTemplateTest {
                 "&leftTicketDTO.to_station=BJP" +
                 "&purpose_codes=ADULT";
         String cookie = "JSESSIONID=D72092789C4E1E331F0667B37A0502AD; _gscu_1094754927=34235684l5zxl714; _trs_uv=jktgbarf_365_9npo";
-        RestTemplate restTemplate = new RestTemplate();
         MultiValueMap<String, Object> paramMap = new LinkedMultiValueMap<>();
         HttpHeaders header = new HttpHeaders();
         header.setContentType(MediaType.APPLICATION_JSON);
